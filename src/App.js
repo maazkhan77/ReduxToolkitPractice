fe3529c152a03ba522ee07004bf644274e5732cb
maazkhan77 <maazkhan7769@gmail.com>
2023-11-30 14:52:39 +0530

    added user page and optimization to rerenders

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import {Routes, Route } from "react-router-dom";
+import {Routes, Route, Navigate } from "react-router-dom";
 import AddPostForm from "./features/posts/AddPostForm";
 import PostsList from "./features/posts/PostsList";
 import Layout from "./components/Layout";
 import SinglePostPage from "./features/posts/SinglePostPage";
 import EditPostForm from "./features/posts/EditPostForm";
+import UsersList from "./features/users/UsersList";
+import UserPage from "./features/users/UserPage";
 
 function App() {
   return (
       <Routes>
         <Route path="/" element={<Layout />}>
+
           <Route index element={<PostsList />} />
+
           <Route path="post" element={<AddPostForm />} />
           <Route path="post/:postId" element={<SinglePostPage />} />
           <Route path="post/edit/:postId" element={<EditPostForm />} />
+
+          
+          <Route path="user" element={<UsersList />} />
+          <Route path="user/:userId" element={<UserPage />} />
+
+          {/* Catch all route */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+
         </Route>
       </Routes>
   );
--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,15 +1,9 @@
 import { useSelector } from "react-redux";
-import {
-  selectAllPosts,
-  getPostsError,
-  getPostsStatus,
-  fetchPosts,
-} from "./postsSlice";
+import { selectPostIds, getPostsError, getPostsStatus } from "./postsSlice";
 import PostExcerpt from "./PostExcerpt";
 
 const PostsList = () => {
-
-  const posts = useSelector(selectAllPosts);
+  const orderedPostIds = useSelector(selectPostIds);
   const postsStatus = useSelector(getPostsStatus);
   const error = useSelector(getPostsError);
 
@@ -17,22 +11,14 @@ const PostsList = () => {
   if (postsStatus === "loading") {
     content = <p>Loading...</p>;
   } else if (postsStatus === "succeeded") {
-    const orderedposts = posts
-      .slice()
-      .sort((a, b) => b.date.localeCompare(a.date));
-
-    content = orderedposts.map((post) => (
-      <PostExcerpt post={post} key={post.id} />
+    content = orderedPostIds.map((postId) => (
+      <PostExcerpt postId={postId} key={postId} />
     ));
   } else if (postsStatus === "error") {
     content = <p>{error}</p>;
   }
 
-  return (
-    <section>
-      {content}
-    </section>
-  );
+  return <section>{content}</section>;
 };
 
 export default PostsList;
